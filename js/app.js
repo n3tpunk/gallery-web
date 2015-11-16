02cdd4e2faceb9c012934d8d97080f4b19a18ba6
Marin Marusic <marin@netpunk.net>
2015-11-16 15:08:23 +0100

    Added media section, some stuff on bio, index. Minor JS/SCSS fixes

--- js/app.js
+++ js/app.js
@@ -11,3 +11,16 @@ $(document).ready(function () {
       $('.gallery-container').empty().append($("<img>",{src:this.href}));
     });
 });
+
+function toggleVideo() {
+    if ($('#vplayer:hidden') && $('#aplayer:visible')) {
+        $('#aplayer').css('visibility','hidden').hide();
+        $('#vplayer').css('visibility','visible').show();
+    }
+}
+function toggleAudio() {
+    if ($('#aplayer:hidden') && $('#vplayer:visible')) {
+        $('#vplayer').css('visibility','hidden').hide();
+        $('#aplayer').css('visibility','visible').show();
+    }
+}    
